fc4c72bd447e580e019730bddd43e30c00d09520
p0li3h <67022434+p0li3h@users.noreply.github.com>
2020-10-27 21:11:36 +0100

    Add files via upload

---
+++ p0li3h_client.lua
@@ -0,0 +1,36 @@
+RegisterNetEvent('p0li3h:fix')
+AddEventHandler('p0li3h:fix', function()
+	local playerPed = GetPlayerPed(-1)
+	if IsPedInAnyVehicle(playerPed, false) then
+		local vehicle = GetVehiclePedIsIn(playerPed, false)
+		SetVehicleEngineHealth(vehicle, 1000)
+		SetVehicleEngineOn( vehicle, true, true )
+		SetVehicleFixed(vehicle)
+		notification("~g~Auto Zostalo ~r~Naprawione")
+	else
+		notification("~y~Nie ma cie w pojezdzie !!!")
+	end
+end)
+
+RegisterNetEvent('p0li3h:clean')
+AddEventHandler('p0li3h:clean', function()
+	local playerPed = GetPlayerPed(-1)
+	if IsPedInAnyVehicle(playerPed, false) then
+		local vehicle = GetVehiclePedIsIn(playerPed, false)
+		SetVehicleDirtLevel(vehicle, 0)
+		notification("~g~Auto Zostalo ~r~Wyczyszczone")
+	else
+		notification("~y~Nie ma cie w pojezdzie !!!")
+	end
+end)
+
+RegisterNetEvent('p0li3h:noPerms')
+AddEventHandler('p0li3h:noPerms', function()
+	notification("~r~Nie ~b~Masz ~r~Permisji ~b~Cioto")
+end)
+
+function notification(msg)
+	SetNotificationTextEntry("STRING")
+	AddTextComponentString(msg)
+	DrawNotification(false, false)
+end
---
+++ p0li3h_server.lua
@@ -0,0 +1,47 @@
+local everyoneAllowed = false
+
+local allowed = 
+{
+	"steam:11000011440388f", -- P0LI3H
+	"steam:110000141ecbef2", -- ZAKOLAK
+	"steam:110000103a7cfa1", -- NIKOS
+}
+
+
+AddEventHandler('chatMessage', function(source, n, msg)
+	local msg = string.lower(msg)
+	local identifier = GetPlayerIdentifiers(source)[1]
+	if msg == "/fx" then
+		CancelEvent()
+		if everyoneAllowed == true then
+			TriggerClientEvent('p0li3h:fix', source)
+		else
+			if checkAllowed(identifier) then
+				TriggerClientEvent('p0li3h:fix', source)
+			else
+				TriggerClientEvent('p0li3h:noPerms', source)
+			end
+		end
+	elseif msg == "/c" then 
+		CancelEvent()
+		if everyoneAllowed == true then
+			TriggerClientEvent('p0li3h:clean', source)
+		else
+			if checkAllowed(identifier) then
+				TriggerClientEvent('p0li3h:clean', source)
+			else
+				TriggerClientEvent('p0li3h:noPerms', source)
+			end
+		end
+	end
+end)
+
+function checkAllowed(id)
+	for k, v in pairs(allowed) do
+		if id == v then
+			return true
+		end
+	end
+	
+	return false
+end
